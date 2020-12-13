7fe3c9819317f1db8e98fc73d2abad8301b7264e
明柯 阳 <ouyangmingke@gmail.com>
2020-12-13 11:10:12 +0800

    添加项目文件。

---
+++ MyFistAbpConsole/HelloWorldService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.DependencyInjection;
+
+namespace MyFistAbpConsole
+{
+    /// <summary>
+    /// 服务类     需要把服务注册到容器中
+    /// 继承ITransientDependency接口（瞬态的 生命周期） 其中一种方法
+    /// </summary>
+    class HelloWorldService : ITransientDependency
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ MyFistAbpConsole/MyFirstAbpConsoleAppModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Volo.Abp.Modularity;
+
+namespace MyFistAbpConsole
+{
+    /// <summary>
+    /// 自定义模块  : 需要继承自 抽象类 AbpModule
+    /// </summary>
+    class MyFirstAbpConsoleAppModule : AbpModule
+    {
+    }
+}
